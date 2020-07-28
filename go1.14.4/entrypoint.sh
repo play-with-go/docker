f511f0bfd87d99cce64c47cc82b4efdff512f584
Paul Jolly <paul@myitcv.io>
2020-07-28 11:37:17 +0100

    Add go1.14.6
    Also simplify build to use shared entrypoint.
    
    Add git config to entrypoint

--- go1.14.4/entrypoint.sh
+++ go1.14.4/entrypoint.sh
@@ -1,16 +1,1 @@
-#!/bin/sh
-
-if [ "$#" -eq 0 ]
-then
-  exit
-fi
-USER_UID="${USER_UID:-1000}"
-USER_GID="${USER_GID:-1000}"
-groupadd -r -g $USER_GID gopher
-useradd -s /bin/bash -u $USER_UID -m --no-log-init -r -g gopher gopher
-cat <<'EOD' >> /home/gopher/.bashrc
-export PS1='$ '
-EOD
-cd /home/gopher
-export HOME=/home/gopher
-exec setpriv --reuid gopher --regid gopher --init-groups "$@"
+../entrypoint.sh
