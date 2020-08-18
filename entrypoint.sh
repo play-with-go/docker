e4862733015627f41950a9abe94ac70db8dfffe5
Paul Jolly <paul@myitcv.io>
2020-08-18 15:56:11 +0100

    Add go1.15 and also add PLAYWITHGO_NOGOPHER entry option

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,18 +4,27 @@ if [ "$#" -eq 0 ]
 then
   exit
 fi
-USER_UID="${USER_UID:-1000}"
-USER_GID="${USER_GID:-1000}"
-groupadd -r -g $USER_GID gopher
-useradd -s /bin/bash -u $USER_UID -m --no-log-init -r -g gopher gopher
-cat <<'EOD' >> /home/gopher/.bashrc
+
+# Pick up any mounted CA certs, e.g. mkcert. But don't be noisy about it
+update-ca-certificates > /dev/null 2>&1
+
+if [ "${PLAYWITHGO_NOGOPHER:-}" != "true" ]
+then
+	USER_UID="${USER_UID:-1000}"
+	USER_GID="${USER_GID:-1000}"
+	groupadd -r -g $USER_GID gopher
+	useradd -s /bin/bash -u $USER_UID -m --no-log-init -r -g gopher gopher
+	cat <<'EOD' >> /home/gopher/.bashrc
 export PS1='$ '
 EOD
-cat <<'EOD' >> /home/gopher/.gitconfig
+	cat <<'EOD' >> /home/gopher/.gitconfig
 [user]
-        email = gopher@gopher.com
-        name = Random Gopher
+		  email = gopher@gopher.com
+		  name = Random Gopher
 EOD
-cd /home/gopher
-export HOME=/home/gopher
-exec setpriv --reuid gopher --regid gopher --init-groups "$@"
+	cd /home/gopher
+	export HOME=/home/gopher
+	exec setpriv --reuid gopher --regid gopher --init-groups "$@"
+else
+	"$@"
+fi
