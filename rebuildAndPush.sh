8913ac1356de333cc9245bd0fabed061bb25f182
Paul Jolly <paul@myitcv.io>
2020-08-18 15:57:23 +0100

    Add go1.15 and also add PLAYWITHGO_NOGOPHER entry option (#3)

--- rebuildAndPush.sh
+++ rebuildAndPush.sh
@@ -4,8 +4,9 @@ set -eu
 
 command cd "$( command cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-for i in $(ls -d go*)
+for i in $(find * -mindepth 1 -name Dockerfile -print)
 do
+	i=$(dirname $i)
 	docker build -t playwithgo/$i -f $i/Dockerfile .
 	docker push playwithgo/$i
 done
