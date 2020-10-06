df85b86a06af8d86462f51fbb80806e695dfca73
Paul Jolly <paul@myitcv.io>
2020-10-06 10:08:23 +0100

    Fix Dockerfile user so that Docker copy works (#16)
    An alternative solution to #15.
    
    As part of this change we also:
    
    * drop go1.15 and go1.15.1
    * share the entrypoint file between go* images
    * move the creation of the gopher user to the Dockerfile (along with
      bashrc and gitconfig creation)
    * leave environment-depenent setup to the entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,43 +7,32 @@ then
   exit
 fi
 
+# If we are not root, or we have explicitly disabled doing anything to the
+# gopher user account, simply exec
+if [ "${PLAYWITHGO_NOGOPHER:-}" = "true" ]
+then
+	exec "$@"
+fi
+
 # Pick up any mounted CA certs, e.g. mkcert. But don't be noisy about it
 if [ "${GOPHERLIVE_ROOTCA:-}" != "" ]
 then
-	echo "$GOPHERLIVE_ROOTCA" > /usr/local/share/ca-certificates/playwithgo_rootca.crt
-	update-ca-certificates > /dev/null 2>&1
+	echo "$GOPHERLIVE_ROOTCA" | sudo cp /dev/stdin /usr/local/share/ca-certificates/playwithgo_rootca.crt
+	sudo update-ca-certificates > /dev/null 2>&1
 fi
 
-if [ "${PLAYWITHGO_NOGOPHER:-}" != "true" ]
+# Otherwise, we are root and expected to ultimately exec as gopher.
+# Do some environment setup then exec as gopher
+if [ "${GITEA_USERNAME:-}" != "" ] && [ "${GITEA_PASSWORD:-}" != "" ]
 then
-	USER_UID="${USER_UID:-1000}"
-	USER_GID="${USER_GID:-1000}"
-	groupadd -r -g $USER_GID gopher
-	useradd -s /bin/bash -u $USER_UID -m --no-log-init -r -g gopher gopher
-	cat <<'EOD' >> /home/gopher/.bashrc
-export PS1='$ '
-EOD
-	cat <<'EOD' >> /home/gopher/.gitconfig
-[user]
-		  email = gopher@gopher.com
-		  name = Random Gopher
-[init]
-		  defaultBranch = main
-EOD
-	if [ "${GITEA_USERNAME:-}" != "" ] && [ "${GITEA_PASSWORD:-}" != "" ]
-	then
-		cat <<EOD >> /home/gopher/.netrc
+	cat <<EOD >> /home/gopher/.netrc
 machine gopher.live
 login $GITEA_USERNAME
 password $GITEA_PASSWORD
 
 EOD
-		chown gopher:gopher /home/gopher/.netrc
-		chmod 600 /home/gopher/.netrc
-	fi
-	cd /home/gopher
-	export HOME=/home/gopher
-	exec setpriv --reuid gopher --regid gopher --init-groups "$@"
-else
-	exec "$@"
+	chmod 600 /home/gopher/.netrc
 fi
+cd /home/gopher
+export HOME=/home/gopher
+exec "$@"
