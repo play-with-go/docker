47c6a402315eacd73376e260a24a6702bf4ee4c9
Paul Jolly <paul@myitcv.io>
2020-11-04 06:43:51 +0000

    go: do not set GOPATH
    Ensure GOPATH is unset; this gives a situation more realistic to a
    first-time user.
    
    Also tweak the rebuildAndPush.sh script to have a -b build-only flag.

--- rebuildAndPush.sh
+++ rebuildAndPush.sh
@@ -1,6 +1,20 @@
 #!/usr/bin/env bash
 
-set -eu
+set -euo pipefail
+shopt -s inherit_errexit
+
+push=true
+
+while getopts ":b" opt; do
+  case $opt in
+    b)
+      push=false
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      ;;
+  esac
+done
 
 command cd "$( command cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
@@ -8,6 +22,9 @@ for i in $(find * -mindepth 1 -name Dockerfile -print)
 do
 	i=$(dirname $i)
 	docker build -t playwithgo/$i -f $i/Dockerfile .
-	docker push playwithgo/$i
+	if [ "$push" == "true" ]
+	then
+		docker push playwithgo/$i
+	fi
 done
 
