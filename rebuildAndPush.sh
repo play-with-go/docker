91e7ebf13dfd6d07c28d1f2982ae7eace0ba9158
Paul Jolly <paul@myitcv.io>
2022-01-04 05:41:53 +0000

    all: multi-arch image support
    All development of PWG had previously been done on amd64 machines. This
    meant we had baked in lots of assumptions about base image digests.
    
    For things to work fast and well on non-amd64 machines, we need to
    switch to a platform-agnostic approach.
    
    Switch the docker build process to target linux/arm64 and linux/amd64.
    
    Also add a basic GitHub workflow that builds all the images.

--- rebuildAndPush.sh
+++ rebuildAndPush.sh
@@ -6,28 +6,48 @@ shopt -s inherit_errexit
 export COMPOSE_DOCKER_CLI_BUILD=1
 export DOCKER_BUILDKIT=1
 
-push=true
+pushOrLoad="--push"
+platform="linux/amd64,linux/arm64"
+optb=false
+optl=false
 
-while getopts ":b" opt; do
+while getopts ":bl" opt; do
   case $opt in
     b)
-      push=false
+		optb=true
+      pushOrLoad=""
+      ;;
+    l)
+		optl=true
+      pushOrLoad="--load"
+		platform="$(go env GOOS)/$(go env GOARCH)"
       ;;
     \?)
       echo "Invalid option: -$OPTARG" >&2
       ;;
   esac
 done
+shift $(($OPTIND - 1))
+
+if $optb && $optl
+then
+	echo "cannot specify -b and -l"
+	exit 1
+fi
 
 command cd "$( command cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
-for i in $(find * -mindepth 1 -name Dockerfile -print)
+files=$(find * -mindepth 1 -name Dockerfile -print)
+if [ "$#" != 0 ]
+then
+	files="$@"
+fi
+
+for i in $files
 do
 	i=$(dirname $i)
-	docker build -t playwithgo/$i -f $i/Dockerfile .
-	if [ "$push" == "true" ]
-	then
-		docker push playwithgo/$i
-	fi
+	cmd="docker buildx build $pushOrLoad -t playwithgo/$i --platform $platform -f $i/Dockerfile ."
+	echo "Running: $cmd"
+	$cmd
 done
 
